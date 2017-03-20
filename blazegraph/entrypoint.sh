018b78887adf53a52ad629128d5061fb60160952
Jason Koh <bk7749@gmail.com>
2017-03-19 23:54:10 -0700

    optimizing config params

--- blazegraph/entrypoint.sh
+++ blazegraph/entrypoint.sh
@@ -2,7 +2,7 @@
 
 bash /bin/file_loader.sh &
 
-java -server -Xmx4g -Djetty.port=9998 -jar blazegraph.jar /opt/fullfeature.properties
+java -server -XX:+UseG1GC -Xmx4g -Djetty.port=9998 -jar blazegraph.jar /opt/fullfeature.properties
 
 #curl -D- -H 'Content-Type: text/turtle' --upload-file /data/Brick.ttl -X POST 'http://localhost:9999/blazegraph/sparql'
 #curl -D- -H 'Content-Type: text/turtle' --upload-file /data/Berkeley.ttl -X POST 'http://localhost:9999/blazegraph/sparql'
