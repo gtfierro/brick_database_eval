a2ec0e9f4f705e71d2defee6162b25da58aa3fb2
Gabe Fierro <gtfierro225@gmail.com>
2017-05-14 17:17:12 -0700

    add more queries

--- rdfrun/queries.py
+++ rdfrun/queries.py
@@ -119,3 +119,347 @@ spatial['hod'] = hodify(spatial['sparql'])
 
 queries['spatial layout query'] = spatial
 varorder['spatial layout query'] = ['?floor','?room','?zone']
+
+
+#### QUERY 4
+q4 = {}
+q4['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE { ?x rdf:type brick:Room . }
+""".strip()
+q4['hod'] = hodify(q4['sparql'])
+
+queries['room enumerate'] = q4
+varorder['room enumerate'] = ['?x']
+
+
+#### QUERY 5
+q5 = {}
+q5['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?vav ?room WHERE {
+    ?vav rdf:type brick:VAV .
+    ?room rdf:type brick:Room .
+    ?zone rdf:type brick:HVAC_Zone .
+    ?vav bf:feeds+ ?zone .
+    ?room bf:isPartOf ?zone .
+}
+""".strip()
+q5['hod'] = hodify(q5['sparql'])
+
+queries['vav/room/zone'] = q5
+varorder['vav/room/zone'] = ['?vav', '?room']
+
+#### QUERY 6
+q6 = {}
+q6['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?pred ?obj WHERE {
+    ?vav rdf:type brick:VAV .
+    ?vav ?pred ?obj .
+}
+""".strip()
+q6['hod'] = hodify(q6['sparql'])
+
+queries['vav triples'] = q6
+varorder['vav triples'] = ['?pred','?obj']
+
+#### QUERY 7
+q7 = {}
+q7['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?x rdf:type brick:Room .
+}
+""".strip()
+q7['hod'] = hodify(q7['sparql'])
+queries['q7'] = q7
+varorder['q7'] = ['?x']
+
+#### QUERY 8
+q8 = {}
+q8['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds ?x .
+}
+""".strip()
+q8['hod'] = hodify(q8['sparql'])
+queries['q8'] = q8
+varorder['q8'] = ['?x']
+
+#### QUERY 9
+q9 = {}
+q9['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds+ ?x .
+}
+""".strip()
+q9['hod'] = hodify(q9['sparql'])
+queries['q9'] = q9
+varorder['q9'] = ['?x']
+
+#### QUERY 10
+q10 = {}
+q10['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?x bf:isFedBy+ ?ahu .
+}
+""".strip()
+q10['hod'] = hodify(q10['sparql'])
+queries['q10'] = q10
+varorder['q10'] = ['?x']
+
+#### QUERY 11
+q11 = {}
+q11['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds/bf:feeds ?x .
+}
+""".strip()
+q11['hod'] = hodify(q11['sparql'])
+queries['q11'] = q11
+varorder['q11'] = ['?x']
+
+#### QUERY 12
+q12 = {}
+q12['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds/bf:feeds+ ?x .
+}
+""".strip()
+q12['hod'] = hodify(q12['sparql'])
+queries['q12'] = q12
+varorder['q12'] = ['?x']
+
+#### QUERY 13
+q13 = {}
+q13['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds/bf:feeds? ?x .
+}
+""".strip()
+q13['hod'] = hodify(q13['sparql'])
+queries['q13'] = q13
+varorder['q13'] = ['?x']
+
+#### QUERY 14
+q14 = {}
+q14['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?x bf:isFedBy/bf:isFedBy? ?ahu .
+}
+""".strip()
+q14['hod'] = hodify(q14['sparql'])
+queries['q14'] = q14
+varorder['q14'] = ['?x']
+
+#### QUERY 15
+q15 = {}
+q15['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?ahu bf:feeds* ?x .
+}
+""".strip()
+q15['hod'] = hodify(q15['sparql'])
+queries['q15'] = q15
+varorder['q15'] = ['?x']
+
+#### QUERY 16
+q16 = {}
+q16['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?x WHERE {
+    ?ahu rdf:type brick:AHU .
+    ?x bf:isFedBy* ?ahu .
+}
+""".strip()
+q16['hod'] = hodify(q16['sparql'])
+queries['q16'] = q16
+varorder['q16'] = ['?x']
+
+#### QUERY 17
+q17 = {}
+q17['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?vav ?room WHERE {
+    ?vav rdf:type brick:VAV .
+    ?room rdf:type brick:Room .
+    ?zone rdf:type brick:HVAC_Zone .
+    ?vav bf:feeds+ ?zone .
+    ?room bf:isPartOf ?zone .
+}
+""".strip()
+q17['hod'] = hodify(q17['sparql'])
+queries['q17'] = q17
+varorder['q17'] = ['?vav','?room']
+
+#### QUERY 18
+q18 = {}
+q18['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?sensor WHERE {
+    ?sensor rdf:type/rdfs:subClassOf* brick:Zone_Temperature_Sensor .
+}
+""".strip()
+q18['hod'] = hodify(q18['sparql'])
+queries['q18'] = q18
+varorder['q18'] = ['?sensor']
+
+#### QUERY 19
+q19 = {}
+q19['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?sensor ?room WHERE {
+    ?sensor rdf:type/rdfs:subClassOf* brick:Zone_Temperature_Sensor .
+    ?room rdf:type brick:Room .
+    ?vav rdf:type brick:VAV .
+    ?zone rdf:type brick:HVAC_Zone .
+    ?vav bf:feeds+ ?zone .
+    ?zone bf:hasPart ?room .
+    ?sensor bf:isPointOf ?vav .
+}
+""".strip()
+q19['hod'] = hodify(q19['sparql'])
+queries['q19'] = q19
+varorder['q19'] = ['?sensor','?room']
+
+#### QUERY 20
+q20 = {}
+q20['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?sensor ?room WHERE {
+ ?sensor rdf:type/rdfs:subClassOf* brick:Zone_Temperature_Sensor .
+ ?vav rdf:type brick:VAV .
+ ?zone rdf:type brick:HVAC_Zone .
+ ?room rdf:type brick:Room .
+ ?vav bf:feeds+ ?zone .
+ ?zone bf:hasPart ?room .
+ {
+    { ?sensor bf:isPointOf ?vav  }
+    UNION
+    { ?sensor bf:isPointOf ?room  }
+ }
+}
+""".strip()
+q20['hod'] = """
+SELECT ?sensor ?room WHERE {
+ ?sensor rdf:type/rdfs:subClassOf* brick:Zone_Temperature_Sensor .
+ ?vav rdf:type brick:VAV  .
+ ?zone rdf:type brick:HVAC_Zone  .
+ ?room rdf:type brick:Room  .
+ ?vav bf:feeds+ ?zone  .
+ ?zone bf:hasPart ?room  .
+ {
+    { ?sensor bf:isPointOf ?vav .}
+    OR
+    { ?sensor bf:isPointOf ?room .}
+ }
+};
+"""
+queries['q20'] = q20
+varorder['q20'] = ['?sensor','?room']
+
+#### QUERY 21
+q21 = {}
+q21['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?sensor ?room WHERE {
+    ?sensor rdf:type/rdfs:subClassOf* brick:Zone_Temperature_Sensor .
+    ?room rdf:type brick:Room .
+    ?vav rdf:type brick:VAV .
+    ?zone rdf:type brick:HVAC_Zone .
+    ?vav bf:feeds+ ?zone .
+    ?zone bf:hasPart ?room .
+    ?sensor bf:isPointOf ?room .
+}
+""".strip()
+q21['hod'] = hodify(q21['sparql'])
+queries['q21'] = q21
+varorder['q21'] = ['?sensor','?room']
+
+#### QUERY 22
+q22 = {}
+q22['sparql'] = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX brick: <http://buildsys.org/ontologies/Brick#>
+PREFIX bf: <http://buildsys.org/ontologies/BrickFrame#>
+SELECT ?vav ?x ?y WHERE {
+    ?vav rdf:type brick:VAV .
+    ?vav bf:hasPoint ?x .
+    ?vav bf:isFedBy ?y .
+}
+""".strip()
+q22['hod'] = hodify(q22['sparql'])
+queries['q22'] = q22
+varorder['q22'] = ['?vav','?x','?y']
