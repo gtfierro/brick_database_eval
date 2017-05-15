1da7d5296555043b5520e9a0f46659b4d563215b
Gabe Fierro <gtfierro225@gmail.com>
2017-05-15 13:18:24 -0700

    correct hodify() because we can support PREFIX now

--- rdfrun/util.py
+++ rdfrun/util.py
@@ -3,9 +3,4 @@ def hodify(qstr):
     Strip PREFIX, add ';'
     """
     qstr = qstr.replace('DISTINCT','')
-    lines = qstr.split('\n')
-    res = []
-    for l in lines:
-        if l.strip().startswith('PREFIX'): continue
-        res.append(l)
-    return '\n'.join(res)+';'
+    return qstr+';'
