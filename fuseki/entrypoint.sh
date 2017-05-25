28d5ef6b5e06d9d2122bbcc674b81fd819e9e700
Gabe Fierro <gtfierro225@gmail.com>
2017-05-25 15:05:20 -0700

    correctly merge datasets for fuseki

--- fuseki/entrypoint.sh
+++ fuseki/entrypoint.sh
@@ -8,10 +8,11 @@ sed -i -e 's/\(.*localhostFilter\)/#\1/' run/shiro.ini
 mkdir DB
 ./fuseki-server --loc=DB /berkeley & 
 pid=$!
-sleep 5
-bin/s-put http://localhost:3030/berkeley/data default /Brick.ttl
-bin/s-put http://localhost:3030/berkeley/data default /BrickFrame.ttl
-bin/s-put http://localhost:3030/berkeley/data default /berkeley.ttl
 kill $pid
+sleep 5
+/opt/jena/apache-jena-3.1.0/bin/tdbloader -loc=DB /Brick.ttl /BrickFrame.ttl /berkeley.ttl
+#bin/s-put http://localhost:3030/berkeley/data default /Brick.ttl
+#bin/s-put http://localhost:3030/berkeley/data default /BrickFrame.ttl
+#bin/s-put http://localhost:3030/berkeley/data default /berkeley.ttl
 sleep 1
 ./fuseki-server --loc=DB /berkeley
